b979567476eaefbec972c201ec2cd67166ad4b79
Moctar <moctarjallo@gmail.com>
2025-05-25 01:16:22 +0000

    deployed successfully to render.com

--- client.py
+++ client.py
@@ -2,7 +2,8 @@ import requests
 
 
 # url = "http://127.0.0.1:8000/sentiment"
-url = "http://0.0.0.0:4000/sentiment"
+# url = "http://0.0.0.0:4000/sentiment"
+url = "https://loopl-assessment.onrender.com/sentiment"
 
 data = {
     "phrase": "Ce produit est fantastique, je le recommande vivement !",
